07eb43633b7c43581e9b2a44c74e7fb476e74a25
6nobel <6nobel@users.noreply.github.com>
2020-04-07 14:33:02 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -82,15 +82,17 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    //use of a temporary array to swap values
     int temp[3];
-    for (int i = 0; i < (height); i++)
+    for (int j = 0; j < height; j++)
     {
-        for (int j = 0; j < (width / 2); j++)
+        for (int i = 0; i < width / 2; i++)
         {
             temp[0] = image[j][i].rgbtBlue;
             temp[1] = image[j][i].rgbtGreen;
             temp[2] = image[j][i].rgbtRed;
 
+            // swap pixels with the ones on the opposite side of the picture and viceversa
             image[j][i].rgbtBlue = image[j][width - i - 1].rgbtBlue;
             image[j][i].rgbtGreen = image[j][width - i - 1].rgbtGreen;
             image[j][i].rgbtRed = image[j][width - i - 1].rgbtRed;
