2bc05e3d8e49bbdd8c03ee38c000a8f18a1518f6
6nobel <6nobel@users.noreply.github.com>
2020-04-10 13:15:33 +0000

    automated commit by submit50

--- readability.py
+++ readability.py
@@ -1,23 +1,24 @@
 from cs50 import get_string
 
+# prompt user to give text
 string = get_string("text: ")
 string = string.replace("'", "")
 string = string.replace('"', '')
 
+# initilize counters
 words = 0
 sentences = 0
 letters = 0
 
+# fill counters
 sentences = string.count('.') + string.count('?') + string.count('!')
 letters = len(string) - string.count(' ') - string.count('.') - string.count('?') - string.count('!') - string.count(',')
 words = string.count(" ") + 1
 
-print (sentences)
-print (letters)
-print (words)
+# pluf them into the formula
+grade = 0.0588 * letters / words * 100 - 0.296 * sentences / words * 100 - 15.8
 
-grade = 0.0588 * letters / words * 100 - 0.296 * sentences / words *100 - 15.8
-
+# print result
 if grade > 16:
     print("Grade 16+")
 elif grade < 1:
